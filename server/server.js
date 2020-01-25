e8cdd07577487bd6414f45eafe528a91265c2157
unknown <saroyan24@gmail.com>
2020-01-25 05:05:22 +0330

    User register post method added

--- server/server.js
+++ server/server.js
@@ -5,6 +5,8 @@
 process.env.NODE_CONFIG_DIR = __dirname + '/config';
 
 const config = require('config');
+const express = require('express');
+const _ = require('lodash');
 
  const {User} = require('./model/user');
 
@@ -13,13 +15,38 @@ console.log(config.get('MONGOURI'));
 console.log(config.get('PORT'));
 
 
-let newUser = new User({
-    fullname: 'Sarmen Saroyan',
-    email: 'novin@gmail.com',
-    password: '123321'
+// let newUser = new User({
+//     fullname: 'Sarmen Saroyan',
+//     email: 'novin@gmail.com',
+//     password: '123321'
+// });
+
+// newUser.save().then((user) => {
+//     console.log('User has been saved to the database', user);
+// });
+
+const app = express();
+app.use(express.json());
+
+app.post('/api/users', (req, res) => {
+    const body = _.pick(req.body, ['fullname', 'email', 'password']);
+
+    console.log(body);
+
+    let user = new User(body);
+
+    user.save().then((user) => {
+       res.status(200).send(user); 
+    }, (err) => {
+        res.status(400).json({
+            Error : `Something went wrong. ${err}`
+        });
+    });
+
 });
 
-newUser.save().then((user) => {
-    console.log('User has been saved to the database', user);
+
+app.listen(config.get('PORT'), () => {
+    console.log(`Server is running on port ${config.get('PORT')}`)
 });
 
